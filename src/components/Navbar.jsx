813556387384396da6770573e8c72576bd6fb88c
mphoChuene <mphochuene42@gmail.com>
2023-08-01 12:48:41 +0200

    navbar in progress

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 // import "./index.css";
 
 const Navbar = () => {
+  const [nav, useNav] = useState(false);
+
+  const handleNav = () => {
+    useNav(!nav);
+  };
+
   return (
     <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
       <h1 className="w-full text-3xl font-bold text-[#00df9a]">
@@ -15,15 +21,18 @@ const Navbar = () => {
         <li className="p-4">About</li>
         <li className="p-4">Contact</li>
       </ul>
-      <div>
-        <AiOutlineMenu size={20} />
+      <div onClick={handleNav}>
+        {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
       </div>
-      <div className="fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-600 m-4">
+      <div className="fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] m-4">
+        <h1 className="w-full text-3xl font-bold text-[#00df9a]">
+          Whit3man Graphics
+        </h1>
         <ul className="uppercase pt-24 text-white">
-          <li className="p-4">Home</li>
-          <li className="p-4">Company</li>
-          <li className="p-4">Resources</li>
-          <li className="p-4">About</li>
+          <li className="p-4 border-b border-gray-600">Home</li>
+          <li className="p-4 border-b border-gray-600">Company</li>
+          <li className="p-4 border-b border-gray-600">Resources</li>
+          <li className="p-4 border-b border-gray-600">About</li>
           <li className="p-4">Contact</li>
         </ul>
       </div>
