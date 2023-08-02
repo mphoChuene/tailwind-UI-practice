acc067e877de8d987089d1ef02928a5bc0c33177
mphoChuene <mphochuene42@gmail.com>
2023-08-02 12:37:49 +0200

    navbar completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,17 +14,23 @@ const Navbar = () => {
       <h1 className="w-full text-3xl font-bold text-[#00df9a]">
         Whit3man Graphics
       </h1>
-      <ul className="flex hidden ">
+      <ul className="hidden md:flex">
         <li className="p-4">Home</li>
         <li className="p-4">Company</li>
         <li className="p-4">Resources</li>
         <li className="p-4">About</li>
         <li className="p-4">Contact</li>
       </ul>
-      <div onClick={handleNav}>
+      <div onClick={handleNav} className="block md:hidden">
         {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
       </div>
-      <div className="fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] m-4">
+      <div
+        className={
+          !nav
+            ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] m-4 ease-in-out duration-500"
+            : "fixed left-[-100%]"
+        }
+      >
         <h1 className="w-full text-3xl font-bold text-[#00df9a]">
           Whit3man Graphics
         </h1>
